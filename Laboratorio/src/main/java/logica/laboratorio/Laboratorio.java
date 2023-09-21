e161db993694e8a59941c43aa18394f7eaa40441
Naidelyn Gutierrez <naidelyngutierrez13@gmail.com>
2023-09-20 22:26:32 -0600

    GUI Workshop

---
+++ Laboratorio/src/main/java/logica/laboratorio/Laboratorio.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package logica.laboratorio;
+
+
+/**
+ *
+ * @author Usuario
+ */
+public class Laboratorio {
+
+    public static void main(String[] args) {
+        Ventana ventana = new Ventana();
+        ventana.setVisible(true);
+    }
+}
